00daf951bbc549e5f0fc7a14bb9e815d7dd99ec7
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2026-07-03 00:55:39 -0300

    feat: adicionar gerenciamento de benefícios do ingresso VIP

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -1,13 +1,54 @@
 package service;
 
+import exceptions.TicketNotFoundException;
+import model.Ticket;
+
+import java.util.List;
+
 public class TicketService {
 
     public boolean validateDescription(String description) {
-        return description.length() <= 1000 && !description.isEmpty();
+        return !description.isEmpty() && description.length() <= 1000;
     }
 
     public boolean validatePrice(double price) {
         return price > 0;
+    }
+
+    public boolean validateQuantity(int quantity) {
+        return quantity > 0;
+    }
+
+    public boolean validateBenefit(String benefit) {
+        return !benefit.isEmpty() && benefit.length() <= 1000;
+    }
+
+    public void hasTicket(Ticket ticket) {
+        if (ticket == null) {
+            throw new TicketNotFoundException();
+        }
+    }
+
+    public boolean validateBenefitId(int newBenefitId, List<String> benefits) {
+        return newBenefitId > 0 && newBenefitId <= benefits.size();
+    }
+
+    public void addBenefit(List<String> benefits, String benefit) {
+        benefits.add(benefit);
+    }
+
+    public void listBenefits(List<String> benefits) {
+        for (String benefit : benefits) {
+            System.out.println(benefit);
+        }
+    }
+
+    public void editBenefit(List<String> benefits, int benefitId, String newBenefit) {
+        benefits.set(benefitId, newBenefit);
+    }
+
+    public void deleteBenefit(List<String> benefits, int benefitId) {
+        benefits.remove(benefitId);
     }
 
 }
