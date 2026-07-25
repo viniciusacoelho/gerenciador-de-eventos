dd2304f091418ab8a7bbbad9c560a67a8c9f5a44
Vinícius Araújo Coelho <viniciusaccoelho123@gmail.com>
2026-07-25 15:55:39 -0300

    docs: reorganizar localização da documentação JavaDoc

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"app"},{"l":"enums"},{"l":"exceptions"},{"l":"model"},{"l":"repository"},{"l":"service"},{"l":"util"},{"l":"view"}];updateSearchResults();
